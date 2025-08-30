d37b581de82b1982f46640257f22611cec25ae7b
Codingtime <13tia.vora@dulwich-seoul.kr>
2025-08-30 10:50:21 +0900

    reversestr

---
+++ reversestr.py
@@ -0,0 +1,10 @@
+def reverseString(s):
+    if len(s) == 1:
+        return s[0]
+    
+    firstchar = s[0]
+
+    return reverseString(s[1:])+firstchar
+
+s = "Good morning!"
+print(reverseString(s))
